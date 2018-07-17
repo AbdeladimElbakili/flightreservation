ea4e26bde20e11af5c411bc63f7f334a3b319095
Your Name <you@example.com>
2018-07-18 04:42:30 +0530

    added sql files

---
+++ flights.sql
@@ -0,0 +1,23 @@
+insert into flight values
+(1,'AA1','American Airlines','AUS','NYC',STR_TO_DATE('02-05-2018', '%m-%d-%Y'),'2018-02-05 03:14:07'),
+
+
+(2,'AA2','American Airlines','AUS','NYC',STR_TO_DATE('02-05-2018', '%m-%d-%Y'),'2018-02-05 05:14:07'),
+
+(3,'AA3','American Airlines','AUS','NYC',STR_TO_DATE('02-05-2018', '%m-%d-%Y'),'2018-02-05 06:14:07'),
+
+(4,'SW1','South West','AUS','NYC',STR_TO_DATE('02-05-2018', '%m-%d-%Y'),'2018-02-05 07:14:07'),
+
+(5,'UA1','United Airlines','NYC','DAL',STR_TO_DATE('02-05-2018', '%m-%d-%Y'),'2018-02-05 10:14:07'),
+
+(6,'UA1','United Airlines','NYC','DAL',STR_TO_DATE('02-05-2018', '%m-%d-%Y'),'2018-02-05 10:14:07'),
+
+(7,'SW1','South West','AUS','NYC',STR_TO_DATE('02-06-2018', '%m-%d-%Y'),'2018-02-06 07:14:07'),
+
+(8,'SW2','South West','AUS','NYC',STR_TO_DATE('02-06-2018', '%m-%d-%Y'),'2018-02-06 08:14:07'),
+
+
+(9,'SW3','South West','NYC','DAL',STR_TO_DATE('02-06-2018', '%m-%d-%Y'),'2018-02-06 10:14:07'),
+
+(10,'UA1','United Airlines','NYC','DAL',STR_TO_DATE('02-06-2018', '%m-%d-%Y'),'2018-02-06 10:14:07')
+
